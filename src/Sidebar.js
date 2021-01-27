68e42f7c76725a0992dd85b74d85927518a99992
Bryce Mardon <brycemardon@Bryces-MBP.modem>
2021-01-27 18:25:36 +1100

    new files

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import './Sidebar.css';
+import { Button } from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
 
 function Sidebar() {
     return (
-        <div className="Sidebar">
-            
+        <div className="sidebar">
+            <Button 
+                startIcon={<AddIcon fontSize="large"
+                className="sidebar__compose" />}
+            >
+                Compose
+            </Button>
         </div>
     )
 }
 
-export default Sidebar
+export default Sidebar;
